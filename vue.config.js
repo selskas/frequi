6a54a14198047e1baf4cad69c0f076e8809156df
selskas <andrius.selskas@gmail.com>
2021-01-31 00:24:28 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8081',
+        target: 'http://127.0.0.1:9966',
         changeOrigin: true,
       },
     },
